17f863cc351afa8634c40e514249a0c0928a3fff
juan garassino <juan.garassino@gmail.com>
2024-01-29 15:14:16 +0900

    new main run

--- miniDiffusion/utils/utils.py
+++ miniDiffusion/utils/utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import datetime
 
 from miniDiffusion.utils.params import one_minus_sqrt_alpha_bar, sqrt_alpha_bar, timesteps
 
@@ -37,17 +38,30 @@ def forward_noise(key, x_0, t, verbose=False):
     noisy_image = (reshaped_sqrt_alpha_bar_t * x_0 + reshaped_one_minus_sqrt_alpha_bar_t * noise)
 
     if verbose:
-        # Plot noise and noisy_image
+        # Select one image from the batch
+        image_index = 0  # You can change this index to visualize a different image from the batch
+        single_noise = noise[image_index]
+        single_noisy_image = noisy_image[image_index]
+
+        # Plot the selected image and its corresponding noisy version
         fig, axes = plt.subplots(1, 2, figsize=(10, 5))
-        axes[0].imshow(noise.squeeze(), cmap='gray')
+        axes[0].imshow(single_noise.squeeze(), cmap='gray')
         axes[0].set_title('Noise')
         axes[0].axis('off')
 
-        axes[1].imshow(noisy_image.squeeze(), cmap='gray')
+        axes[1].imshow(single_noisy_image.squeeze(), cmap='gray')
         axes[1].set_title('Noisy Image')
         axes[1].axis('off')
 
-        plt.show()
+        # Generate a timestamp for the plot filename
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+        # Save the plot with timestamp in the filename
+        plot_filename = f"noisy_image_plot_{timestamp}.png"
+        plt.savefig(plot_filename)
+
+        # Close the plot to avoid cache problems
+        plt.close()
 
     return noisy_image, noise
 
